7ef06dc033308fa939c03d508f22b57c09f610b0
Luis German <LuisGermanRT@gmail.com>
2019-10-26 03:45:35 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// error handler
+app.use(function(req, res) {
+	// set locals, only providing error in development
+	res.locals.message = '404 Not Found';
+
+	// render the error page
+	res.status(404);
+	res.render('error');
+});
+
+module.exports = app;
